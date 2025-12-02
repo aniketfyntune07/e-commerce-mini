a82504258f17cd57a65d87dfcf9d7e11a5549ba5
raj-kale <rajkale715@gmail.com>
2025-12-02 15:59:06 +0530

    feat- added track order feature for admin

---
+++ app/Http/Controllers/AdminOrderController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class AdminOrderController extends Controller
+{
+    public function index()
+    {
+        $orders=Order::orderBy('created_at', 'desc')->paginate(10);
+        return view('admin.orders.index', compact('orders'));
+    }
+
+    public function show(Order $order)
+    {
+        return view('admin.orders.show', compact('order'));
+    }
+}
